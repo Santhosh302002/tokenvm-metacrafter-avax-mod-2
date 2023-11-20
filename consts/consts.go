1a971fc153645a0953425b3cbc5d7933b45237df
Santhosh302002 <l.santhoshkumar30@gmail.com>
2023-11-20 18:42:44 +0530

    completed👍

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "token"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "TokenVM"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "TKNVM"
 )
 
 var ID ids.ID
